e48591de300c3b850ab36d0066cd40293765c542
Tanin Rahman <taninr87@gmail.com>
2022-10-24 22:28:39 +0600

    install all dependencies

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBmUMWPeb80shOkWZKqlrBRXwA67JbF4JE",
+  authDomain: "study-trend-ecdf6.firebaseapp.com",
+  projectId: "study-trend-ecdf6",
+  storageBucket: "study-trend-ecdf6.appspot.com",
+  messagingSenderId: "450702798557",
+  appId: "1:450702798557:web:6034f70a77f1a5c4d8b534"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
