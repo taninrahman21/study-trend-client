37ac33d7663c8d767d900093ad2754da10761ccf
Tanin Rahman <taninr87@gmail.com>
2022-10-26 12:26:07 +0600

    sticky course nav and navbar

--- src/Pages/CourseLayout/CourseLayout.js
+++ src/Pages/CourseLayout/CourseLayout.js
@@ -6,26 +6,34 @@ import { NavLink, Outlet } from "react-router-dom";
 const CourseLayout = () => {
   const [courses, setCourses] = useState([]);
 
-  useEffect( () => {
-    fetch('http://localhost:5000/courses')
-    .then(res => res.json())
-    .then(data => setCourses(data))
-  }, [])
+  useEffect(() => {
+    fetch("http://localhost:5000/courses")
+      .then((res) => res.json())
+      .then((data) => setCourses(data));
+  }, []);
 
   return (
     <div className="flex w-full pl-[80px] bg-gray-200">
       <div className="w-2/5 py-5 text-2xl">
-      {
-        courses.map(course => <NavLink
-          key={course.id} 
-          to={`/courses/${course.id}`}
-          className={({ isActive }) => isActive ? 'block my-5 font-semibold text-red-800' : 'font-normal block my-5'}
-          >{course.name}</NavLink>)
-      }
+        <div className="sticky top-24">
+          {courses.map((course) => (
+            <NavLink
+              key={course.id}
+              to={`/courses/${course.id}`}
+              className={({ isActive }) =>
+                isActive
+                  ? "block my-5 font-semibold text-red-800"
+                  : "font-normal block my-5"
+              }
+            >
+              {course.name}
+            </NavLink>
+          ))}
+        </div>
       </div>
 
       <div className="bg-white px-8 w-full">
-       <Outlet></Outlet>
+        <Outlet></Outlet>
       </div>
     </div>
   );
